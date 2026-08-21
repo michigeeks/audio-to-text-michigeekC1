81b88383a869b370f69c3530f8d30e3ee56df593
Michi <michigeeks3@gmail.com>
2026-08-20 21:06:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ from faster_whisper import WhisperModel
 # --------------------------------------------------
 
 st.set_page_config(
-    page_title="Audio → Texto - Michigeeks v1",
+    page_title="Audio → Texto - Michigeeks v2",
     page_icon="🎙️",
     layout="wide",
 )
